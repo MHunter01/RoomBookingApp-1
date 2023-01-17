8c3e0792b7a71586b5e5ebc7d87ae03bef92fd30
MHunter01 <114399652+MHunter01@users.noreply.github.com>
2023-01-17 18:23:44 +0000

    change connection string name & add migration bundle

--- Data/Migrations/20220926110048_UserModelUpdate.Designer.cs
+++ Data/Migrations/20220926110048_UserModelUpdate.Designer.cs
@@ -17,7 +17,7 @@ namespace RoomBookingApp.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.28")
+                .HasAnnotation("ProductVersion", "6.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
